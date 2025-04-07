56f034029072fe9d5817e22c7a2c020e4a635acc
yoavi1 <yoavmeirovitz187@gmail.com>
2025-04-07 22:22:33 +0300

    fix: remove unnecessary semicolon in Thank component

--- my-react-app/src/components/Thank.tsx
+++ my-react-app/src/components/Thank.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Thank = () => {
   return (
     <>
-      <div className="bg-pink-500 text-green-400">thank</div>;
+      <div className="bg-pink-500 text-green-400">thank</div>
     </>
   );
 };
