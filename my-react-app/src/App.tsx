fff678bb148e82284ff0b0ba5e418efffbfc3464
yoavi1 <yoavmeirovitz187@gmail.com>
2025-04-12 00:51:35 +0300

    Add React and JSX Dev Runtime dependencies
    - Introduced `react.js` and `react.js.map` files to manage React imports.
    - Added `react_jsx-dev-runtime.js` and its corresponding source map for JSX development.
    - Implemented the necessary functions and constants for React component handling in the JSX Dev Runtime.

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -3,74 +3,63 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { Thank } from './components/Thank.tsx';
-
+import mobileBg from '../design/ip-address-tracker-master/images/pattern-bg-mobile.png';
+import rArrow from '../design/ip-address-tracker-master/images/icon-arrow.svg';
 function App() {
-  const [rating, setRating] = useState<number | null>(null);
-  const [showThankYou, setShowThankYou] = useState(false);
+  // const [rating, setRating] = useState<number | null>(null);
+  // const [showThankYou, setShowThankYou] = useState(false);
 
-  const btnArr = [1, 2, 3, 4, 5];
+  // const btnArr = [1, 2, 3, 4, 5];
 
-  const handleRatingClick = (value: number) => {
-    setRating(value);
-  };
+  // const handleRatingClick = (value: number) => {
+  //   setRating(value);
+  // };
 
-  const handleSubmit = () => {
-    if (rating !== null) {
-      setShowThankYou(true);
-    }
-  };
+  // const handleSubmit = () => {
+  //   if (rating !== null) {
+  //     setShowThankYou(true);
+  //   }
+  // };
 
   return (
     <>
-      <div className="flex justify-center items-center h-screen bg-black ">
-        {showThankYou ? (
-          <Thank rating={rating} />
-        ) : (
-          <div className="flex flex-col w-[375px] p-4 bg-gray-900 rounded-2xl">
-            <div className=" mb-5 flex justify-center bg-gray-800 p-2 h-7 w-7 rounded-full">
-              <img src="../images/icon-star.svg" alt="" />
-            </div>
-            <h1 className="text-white mb-2">HOW did we do?</h1>
-            <p className="text-gray-500 text-xs leading-4.5">
-              Please let us know how we did with your support request. All feedback is appreciated
-              to help us improve our offering!
-            </p>
-
-            <div className="flex justify-between my-3 ">
-              {btnArr.map((item) => (
-                <button
-                  key={item}
-                  onClick={() => handleRatingClick(item)}
-                  className="hover:cursor-pointer
-                  focus:bg-gray-100
-                  focus:text-gray-950
-                   hover:bg-orange-500
-                    hover:text-gray-950 
-                    text-xs font-medium
-                     text-gray-500
-                      bg-gray-800
-                       w-7 h-7 p-4.5 rounded-full
-                        flex justify-center items-center"
-                  //   className={`
-                  //   hover:bg-orange-500 hover:text-white
-                  //   text-gray-500
-                  //   ${rating === item ? 'bg-gray-500 text-white' : 'bg-gray-800'}
-                  //   w-10 h-10 rounded-full flex justify-center items-center transition-colors
-                  // `}
-                >
-                  {item}
-                </button>
-              ))}
-            </div>
-            <button
-              onClick={handleSubmit}
-              className="bg-white text-gray-950 rounded-full my-2 font-[600] text-[0.725rem] py-1.5"
-            >
-              SUBMIT
-            </button>
+      <section
+        style={{ backgroundImage: `url(${mobileBg})` }}
+        className="flex h-45 bg-cover flex-col items-center px-4  "
+      >
+        <h1 className="text-white m-3 ">ip Adrdress Tracker</h1>
+        <form className="flex max-w-2xl">
+          <input
+            className="bg-white rounded-l-md text-xs w-full p-3 "
+            type="text"
+            name="ipAddress"
+            id="ipAddress"
+            placeholder="  Search for any IP address or domain"
+          />
+          <button className="bg-black p-3 rounded-r-md">
+            <img src={rArrow} alt="" className="" />
+          </button>
+        </form>
+        <div className=" bg-white flex flex-col md:flex-row md:justify-evenly md:text-left text-center w-full mt-4 p-3  rounded-md">
+          <div className="md:border-r-1 border-gray-400 md:pr-12 ">
+            <h2 className="text-xs text-gray-500 font-bold ">IP ADDRESS</h2>
+            <p className="text-sm font-bold">192.212.174.101</p>
           </div>
-        )}
-      </div>
+
+          <div className="md:border-r-1 border-gray-400 md:pr-12">
+            <h2 className="text-xs text-gray-500 font-bold "> Location</h2>
+            <p className="text-sm font-bold">Brooklyn, NY 10001</p>
+          </div>
+          <div className="md:border-r-1 border-gray-400 md:pr-12  ">
+            <h2 className="text-xs text-gray-500 font-bold">Timezone</h2>
+            <p className="text-sm font-bold">UTC -05:00</p>
+          </div>
+          <div className=" md:pr-12 ">
+            <h2 className="text-xs text-gray-500 font-bold">ISP</h2>
+            <p className="text-sm font-bold">SpaceX Starlink</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
